da4e302f425048b2582916c634927ca275b770c7
xiaodeng <927561237@qq.com>
2021-10-28 17:05:14 +0800

    BrowserRouter和HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom' // BrowserRouter 这个标签没有 # ,HashRouter有 # 好
+import { BrowserRouter } from 'react-router-dom' // BrowserRouter 这个标签没有 # ,HashRouter有 # 号 兼容性好
 
 import App from './App'
 

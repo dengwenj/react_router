eeb60b9e24962550337acfb370b4fc9140eb3cae
xiaodeng <927561237@qq.com>
2021-10-28 15:34:46 +0800

    模糊匹配和严格匹配

--- src/App.js
+++ src/App.js
@@ -13,8 +13,12 @@ export default function App() {
       <Link to="/about">about路由</Link>
       <hr />
       <Switch>
+        {/* 路由的模糊匹配和严格匹配， */}
+        {/* 人家要的一样都不能少，而且顺序也不能换（模糊匹配） */}
+        {/* 所输入的东西必须和要匹配的东西完美对应（严格匹配） */}
+        {/* 严格匹配会导致二级路由匹配不上 */}
         <Route path="/home" component={Home} />
-        <Route path="/about" component={About} />
+        <Route exact path="/about" component={About} />
         {/* 这样的话就会一次往下找，找到了也会往下找 加了 Switch 的话找到了就不会往下找了 */}
         {/* <Route path="/home" component={About} />  */}
       </Switch>

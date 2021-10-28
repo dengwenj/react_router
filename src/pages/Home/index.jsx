cdc27242de2793c61b16c32e63b8c4da13ed82ed
xiaodeng <927561237@qq.com>
2021-10-28 15:47:46 +0800

    嵌套路由

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,7 @@ import News from '../News'
 export default class Home extends Component {
   state = {
     style: {
-      height: 100,
+      height: 500,
       backgroundColor: 'skyblue',
     },
   }
@@ -20,6 +20,10 @@ export default class Home extends Component {
         <Link to="/home/news">我是 news 组件</Link>
         <br />
         <Link to="/home/message">我是 message 组件 </Link>
+        {/* 嵌套路由的使用 */}
+        {/* 1 注册子路由时要写上父路由的 path 值 */}
+        {/* 2 路由的匹配是按照注册路由的顺序进行的 */}
+        {/* 重点！！！！！！！！！！！！！每次都要重新从一级路由匹配！！！！！！！！！！！ */}
         <Route path="/home/news" component={News} />
         <Route path="/home/message" component={Message} />
       </div>
--- src/pages/Message/index.jsx
+++ src/pages/Message/index.jsx
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
 
 export default class Message extends Component {
+  state = {
+    arr: [
+      { id: '001', name: 'xiaodeng', age: 18 },
+      { id: '002', name: 'xd', age: 20 },
+      { id: '003', name: 'zhangsan', age: 28 },
+    ],
+    style: {
+      width: 500,
+      height: 200,
+      backgroundColor: 'pink',
+    },
+  }
   render() {
-    return <div>我是message组件，我是home的子组件</div>
+    const { arr, style } = this.state
+    return (
+      <div style={style}>
+        我是message组件，我是home的子组件
+        <ul>
+          {arr.map((item) => {
+            return (
+              <li key={item.id}>
+                姓名：{item.name}-年龄：{item.age}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
   }
 }
